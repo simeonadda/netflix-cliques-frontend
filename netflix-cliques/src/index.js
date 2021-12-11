842831a6accd8a9f27492cf2dd3bbcecd1bd4b31
simboslice <simeonadda@gmail.com>
2021-12-11 11:27:12 -0600

    installed atomize framework

--- netflix-cliques/src/index.js
+++ netflix-cliques/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { StyleReset } from 'atomize';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
 ReactDOM.render(
   <React.StrictMode>
+    <StyleReset />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
